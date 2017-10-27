83b1355836493730ae142f28de767204829725f6
Greencame <julien.mustiere@ntymail.com>
2017-10-27 09:37:58 +0200

    Change component state

--- src/components/Game.js
+++ src/components/Game.js
@@ -39,7 +39,7 @@ class Game extends Component {
         return cells;
     }
 
-    componentWillMount() {
+    componentDidMount() {
         if(this.useLocalStorage)
             this.updateScore();
         else
